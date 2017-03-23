68240b6d25f54a63944d4020b8bde727d35cc6ef
Jorge <jorge_gomez12@hotmail.com>
2017-03-22 20:22:27 -0600

    app

---
+++ KinjoApp/MainMenu.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KinjoApp
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Page
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void goToAppetizer(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Appetizer());
+        }
+
+        private void goToSushi(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Sushi());
+        }
+
+        private void goToSashimi(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Sashimi());
+        }
+
+        private void goToMaki(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Maki());
+        }
+    }
+}
